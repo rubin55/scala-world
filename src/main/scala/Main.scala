9c37b5106c32630bf14848ee02a58ca7678173d4
Rubin Simons <me@rubin55.org>
2023-10-29 20:59:30 +0100

    A main that actually uses Rational.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,5 @@
-@main def hello() = println("Hello, world!")
+@main def hello() =
+  val a = Rational(3,6)
+  val b = Rational(45,93)
+  val c = a + b
+  println(f"A is $a B = $b, they add up to $c")
